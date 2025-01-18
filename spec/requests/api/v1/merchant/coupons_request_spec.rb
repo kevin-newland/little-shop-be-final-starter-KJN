19825f6dbe97a0779a32161044df876bcb169839
Kevin Newland <kevin.newland@yahoo.com>
2025-01-18 15:19:26 -0600

    add tests for getting all coupons for a given merchant

--- spec/requests/api/v1/merchant/coupons_request_spec.rb
+++ spec/requests/api/v1/merchant/coupons_request_spec.rb
@@ -20,9 +20,38 @@ RSpec.describe "Merchant Coupon endpoints" do
     expect(results[:attributes]).to have_key(:name)
     expect(results[:attributes][:name]).to be_a(String)
     expect(results[:attributes][:unique_code]).to be_a(String)
-    expect(results[:attributes][:percent_off]).to be_a(String)
-    expect(results[:attributes][:dollar_off]).to be(nil)
+    expect(results[:attributes][:percent_off]).to be_a(String).or(be_nil)
+    expect(results[:attributes][:dollar_off]).to be_a(Float).or(be_nil)
     expect(results[:attributes][:merchant_id]).to eq(merchant1.id)
     expect(results[:attributes][:times_used]).to be_a(Integer)
+  end
+
+  it "Returns all of a merchants coupons" do
+    merchant1 = Merchant.create!(name: "Test Merchant1")
+    coupon = Coupon.create(name: "Winter Sale", unique_code: "WS2025", percent_off: 15.0, dollar_off: nil, merchant: merchant1)
+    coupon = Coupon.create(name: "Spring Sale", unique_code: "SPS2025", percent_off: nil, dollar_off: 25.00, merchant: merchant1)
+
+    get "/api/v1/merchants/#{merchant1.id}/coupons"
+    expect(response.status).to eq(200)
+
+    results = JSON.parse(response.body, symbolize_names: true)[:data] 
+
+    expect(results.count).to eq(2)
+
+    results.each do |result|
+      expect(result).to have_key(:id)
+      expect(result[:id]).to be_a(String)
+
+      expect(result).to have_key(:type)
+      expect(result[:type]).to be_a(String)
+
+      expect(result).to have_key(:attributes)
+      expect(result[:attributes]).to have_key(:name)
+      expect(result[:attributes][:name]).to be_a(String)
+      expect(result[:attributes][:unique_code]).to be_a(String)
+      expect(result[:attributes][:percent_off]).to be_a(String).or(be_nil)
+      expect(result[:attributes][:dollar_off]).to be_a(Float).or(be_nil)
+      expect(result[:attributes][:merchant_id]).to eq(merchant1.id)
+    end
   end
 end
