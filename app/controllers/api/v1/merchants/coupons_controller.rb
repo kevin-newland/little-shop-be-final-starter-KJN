261b9002287f720fc44752a384ab4baa9444e5f1
Kevin Newland <kevin.newland@yahoo.com>
2025-01-18 17:00:25 -0600

    added create action

--- app/controllers/api/v1/merchants/coupons_controller.rb
+++ app/controllers/api/v1/merchants/coupons_controller.rb
@@ -10,4 +10,16 @@ class Api::V1::Merchants::CouponsController < ApplicationController
     coupons = merchant.coupons.all
     render json: CouponSerializer.new(coupons)
   end
+
+  def create
+    merchant = Merchant.find(params[:merchant_id])
+    coupons = merchant.coupons.create!(coupon_params)
+    render json: CouponSerializer.new(coupons), status: :created
+  end
+
+  private
+
+  def coupon_params
+    params.permit(:name, :unique_code, :percent_off, :dollar_off)
+  end
 end
