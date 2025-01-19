99e32e5aa2152437add1249e8494562ed58e9902
Kevin Newland <kevin.newland@yahoo.com>
2025-01-18 21:22:37 -0600

    updated schema with active

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_17_200108) do
+ActiveRecord::Schema[7.1].define(version: 2025_01_19_003016) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_01_17_200108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "merchant_id", null: false
+    t.boolean "active"
     t.index ["merchant_id"], name: "index_coupons_on_merchant_id"
   end
 
